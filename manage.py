da53dc1e6662bc51da76e7d5b87e744edb3c0a3b
Jeph Do <jdo@solarcity.com>
2014-03-05 23:59:03 -0800

    reorder imports

--- manage.py
+++ manage.py
@@ -1,10 +1,11 @@
-from suggestions.app import app
-from suggestions import models
+
 
 if __name__ == '__main__':
+    from suggestions import models
     models.create_all()
 
     from wsgiref.simple_server import make_server
+    from suggestions.app import app
     server = make_server('0.0.0.0', 8080, app)
     print('Serving at localhost on port 8080...')
     server.serve_forever()
